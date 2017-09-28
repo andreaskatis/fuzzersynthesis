92124f6046ef0b81cbb6a57040812f66eef7a6c8
Grigory Fedyukovich <grigory.fedyukovich@gmail.com>
2017-09-28 13:11:09 -0700

    cmd-option for the skolem generation / printing

--- tools/aeval/Ae.cpp
+++ tools/aeval/Ae.cpp
@@ -8,6 +8,7 @@ using namespace ufo;
  * Usage: specify 2 smt2-files that describe the formula \foral x. S(x) => \exists y . T (x, y)
  *   <s_part.smt2> = S-part (over x)
  *   <t_part.smt2> = T-part (over x, y)
+ *   --skol = to print skolem function
  *
  * Notably, the tool automatically recognizes x and y based on their appearances in S or T.
  *
@@ -19,6 +20,30 @@ using namespace ufo;
  *
  */
 
+bool getBoolValue(const char * opt, bool defValue, int argc, char ** argv)
+{
+  for (int i = 1; i < argc; i++)
+  {
+    if (strcmp(argv[i], opt) == 0) return true;
+  }
+  return defValue;
+}
+
+char * getSmtFileName(int num, int argc, char ** argv)
+{
+  int num1 = 1;
+  for (int i = 1; i < argc; i++)
+  {
+    int len = strlen(argv[i]);
+    if (len >= 5 && strcmp(argv[i] + len - 5, ".smt2") == 0)
+    {
+      if (num1 == num) return argv[i];
+      else num1++;
+    }
+  }
+  return NULL;
+}
+
 int main (int argc, char ** argv)
 {
 
@@ -27,14 +52,15 @@ int main (int argc, char ** argv)
 
   ExprVector params;
 
-  if (argc != 3)
+  if (argc < 3 || argc > 4)
   {
     outs() << "Unable to parse arguments\n";
     return 0;
   }
 
-  aeSolveAndSkolemize(z3_from_smtlib_file (z3, argv [1]),
-                      z3_from_smtlib_file (z3, argv [2]));
+  aeSolveAndSkolemize(z3_from_smtlib_file (z3, getSmtFileName(1, argc, argv)),
+                      z3_from_smtlib_file (z3, getSmtFileName(2, argc, argv)),
+                      getBoolValue("--skol", false, argc, argv));
 
   return 0;
 }
